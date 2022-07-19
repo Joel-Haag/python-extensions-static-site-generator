1d274a1777a43311e2ce33c9d83efc0453d9c82c
Joel Haag <joelohaag@gmail.com>
2022-07-20 01:28:49 +0200

    3rd

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -19,9 +19,7 @@ def written():
 
 @hooks.register("stats")
 def stats():
-    global start_time
-    global total_written
-    final_time = time.time() - start_time()
-    average = final_time / total_written if final_time / total_written != 0 else 0
+    final_time = time.time() - start_time
+    average = final_time / total_written if total_written else 0
     report = "Converted: {} · Time: {:.2f} sec · Avg: {:.4f} sec/file"
-    print(report).format(total_written, final_time, average)
+    print(report.format(total_written, final_time, average))
